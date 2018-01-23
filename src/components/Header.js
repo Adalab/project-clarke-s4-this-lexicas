a3d7f594adca5a0aef3a4b4e97d6aa4cc655c826
GemmaBeferull <gemmabecala@gmail.com>
2018-01-23 21:08:10 +0100

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+      <img src="/images/logoCv.png" alt="Logo" className="logo" />
+      </header>
+
+    );
+  }
+}
+
+export default Header;
