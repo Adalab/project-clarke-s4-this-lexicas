21a344284442a4ee6c791b64a57b652a685fe145
GemmaBeferull <gemmabecala@gmail.com>
2018-01-24 20:27:19 +0100

    js and remove indexhtml

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 class Cv extends Component {
   render() {
     return (
-      <div id="printCv" className="resume-container" onload="window.print();window.close()">
+      <div id="printCv" className="resume-container" onLoad={() => { window.print();window.close()}}>
 
       <section className="resume">
 
       <aside className="resume-left">
-      <img className="profile-picture margin" />
+      <img className="profile-picture margin" alt="Imagen de perfil"/>
       <div className="personal-info-mvl margin">
       <h2 className="title" id="CV0">Nombre</h2>
       <h2 className="title-medium" id="CV0">Profesion</h2>
@@ -24,24 +24,24 @@ class Cv extends Component {
       <div className="contact-info">
       <h4>Contacto</h4>
       <div className="phone-info">
-      <img src alt="" />
-      <p id="CV1" />
+      <img src="" alt="" />
+      <p id="CV1" ></p>
       </div>
       <div className="email-info">
-      <img src alt="" />
-      <p id="CV1" />
+      <img src="" alt="" />
+      <p id="CV1" ></p>
       </div>
       <div className="location-info">
-      <img src alt="" />
-      <p id="CV1" />
+      <img src="" alt="" />
+      <p id="CV1" ></p>
       </div>
       </div>
       <div className="social-media">
-      <p id="social-enlace" />
+      <p id="social-enlace" ></p>
       {/*<p>Linkedin:<a href="https://github.com/Adalab/clarke-s2-varwomen/tree/js-adds">Icono Linkedin</a></p>
       <p>GitHub: @ejemplo</p>*/}
-      <p id="social-enlace" />
-      <p id="social-enlace" />
+      <p id="social-enlace" ></p>
+      <p id="social-enlace" ></p>
       </div>
       </div>
       </aside>
@@ -59,20 +59,20 @@ class Cv extends Component {
       <div className="option">
       <h4 id="work-type-CV">Puesto o Cargo</h4>
       <div className="where-info">
-      <h5 id="company-name-CV" />
+      <h5 id="company-name-CV" ></h5>
       <div className="date-info">
-      <p id="start-work-date" />
-      <p id="end-work-date" />
+      <p id="start-work-date" ></p>
+      <p id="end-work-date" ></p>
       </div>
       </div>
-      <p id="work-description" />
+      <p id="work-description" ></p>
       <div className="work-extra-cv hidden">
       <h4 id="work-type-CV-extra">Puesto o Cargo</h4>
       <div className="where-info">
-      <h5 id="company-name-CV-extra" />
+      <h5 id="company-name-CV-extra" ></h5>
       <div className="date-info">
-      <p id="start-work-date-extra" />
-      <p id="end-work-date-extra" />
+      <p id="start-work-date-extra" ></p>
+      <p id="end-work-date-extra" ></p>
       </div>
       </div>
       <p id="work-description-extra">
@@ -83,21 +83,21 @@ class Cv extends Component {
       <div className="option">
       <h4 id="study-title">Estudios realizados</h4>
       <div className="where-info">
-      <h5 id="school" />
+      <h5 id="school" ></h5>
       <div className="date-info">
-      <p id="start-study-date" />
-      <p id="end-study-date" />
+      <p id="start-study-date" ></p>
+      <p id="end-study-date" ></p>
       </div>
       </div>
       <p id="study-description">
       </p>
       <div className="estudio-extra-cv hidden">
-      <h4 id="study-title-extra" />
+      <h4 id="study-title-extra" ></h4>
       <div className="where-info">
-      <h5 id="school-extra" />
+      <h5 id="school-extra" ></h5>
       <div className="date-info">
-      <p id="start-study-date-extra" />
-      <p id="end-study-date-extra" />
+      <p id="start-study-date-extra" ></p>
+      <p id="end-study-date-extra" ></p>
       </div>
       </div>
       <p id="study-description-extra">
@@ -109,16 +109,16 @@ class Cv extends Component {
       <h4>Idiomas</h4>
       <div className="options-content">
       <div className="options-content-items">
-      <p id="primero-idioma-into-CV" />
-      <p id="idioma-extra1-into-CV" />
-      <p id="idioma-extra2-into-CV" />
-      <p id="idioma-extra3-into-CV" />
+      <p id="primero-idioma-into-CV" ></p>
+      <p id="idioma-extra1-into-CV" ></p>
+      <p id="idioma-extra2-into-CV" ></p>
+      <p id="idioma-extra3-into-CV" ></p>
       </div>
       <div className="options-content-items">
-      <p id="nivel-primero-idioma-into-CV" />
-      <p id="nivel-idioma-extra1-into-CV" />
-      <p id="nivel-idioma-extra2-into-CV" />
-      <p id="nivel-idioma-extra3-into-CV" />
+      <p id="nivel-primero-idioma-into-CV" ></p>
+      <p id="nivel-idioma-extra1-into-CV" ></p>
+      <p id="nivel-idioma-extra2-into-CV" ></p>
+      <p id="nivel-idioma-extra3-into-CV" ></p>
       </div>
       </div>
       </div>
@@ -126,16 +126,16 @@ class Cv extends Component {
       <h4>Skills</h4>
       <div className="options-content">
       <div className="options-content-items">
-      <p id="skill1-cv" />
-      <p id="skill2-cv" />
-      <p id="skill3-cv" />
-      <p id="skill4-cv" />
+      <p id="skill1-cv" ></p>
+      <p id="skill2-cv" ></p>
+      <p id="skill3-cv" ></p>
+      <p id="skill4-cv" ></p>
       </div>
       <div className="options-content-items">
-      <p id="skill1-level-cv" />
-      <p id="skill2-level-cv" />
-      <p id="skill3-level-cv" />
-      <p id="skill4-level-cv" />
+      <p id="skill1-level-cv" ></p>
+      <p id="skill2-level-cv" ></p>
+      <p id="skill3-level-cv" ></p>
+      <p id="skill4-level-cv" ></p>
       </div>
       </div>
       </div>
@@ -145,7 +145,7 @@ class Cv extends Component {
       </div>
       </div>
       </section>
-      <input className="button-print" type="button" onclick="printDiv('printCv')" defaultValue="Imprimir" />
+      <input className="button-print" type="button" onClick="printDiv('printCv')" defaultValue="Imprimir" />
       </div>
 
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import main from './main';
+import legacyCode from './main';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
+legacyCode();
