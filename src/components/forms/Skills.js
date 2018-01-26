4ef1eb59b384b46b654d11a124cc15ce44d05e35
Estela Parrado <estela.parrado@gmail.com>
2018-01-26 01:06:49 +0100

    created skills reusable component and change form-container-items wrapper on sass

--- src/components/forms/Skills.js
+++ src/components/forms/Skills.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Fieldset from '../Fieldset';
+import SkillComponent from './SkillComponent';
 
 class Skills extends Component {
   render() {
     return (
       <Fieldset  formNumber={5} legend="Skills" innerClassname="form-container">
-      <div className="form-container-items">
-      <input type="text" name="skill" id="skill1" defaultValue placeholder="Skill" />
-      </div>
-      <div className="form-container-items">
-      <select name="nivel" id="nivel-skill1">
-      <option value />
-      <option selected disabled value="Seleccionar nivel">Seleccionar nivel</option>
-      <option value="Beginner">Beginner</option>
-      <option value="Conversational">Elementary</option>
-      <option value="Fluent">Intermediate</option>
-      <option value="Native">Advanced</option>
-      </select>
-      </div>
+      <SkillComponent />
+      <SkillComponent />
+      <SkillComponent />
       </Fieldset>
     );
   }
